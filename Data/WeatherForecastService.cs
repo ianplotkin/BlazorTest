02c23283a77c2a81eea4b09e8abee74a731d8346
Ian Plotkin <dev1@ianplotkin.com>
2021-08-28 11:32:47 -0400

    We can add, edit and remove items!

--- Data/WeatherForecastService.cs
+++ Data/WeatherForecastService.cs
@@ -25,5 +25,55 @@ namespace BlazorTest.Data
                  // Use ToListAsync to avoid blocking a thread
                  .AsNoTracking().ToListAsync();
         }
+
+        public Task<WeatherForecast> CreateForecastAsync(WeatherForecast objWeatherForecast)
+        {
+            _context.WeatherForecast.Add(objWeatherForecast);
+            _context.SaveChanges();
+            return Task.FromResult(objWeatherForecast);
+        }
+
+        public Task<bool> UpdateForecastAsync(WeatherForecast objWeatherForecast)
+        {
+            var ExistingWeatherForecast =
+                _context.WeatherForecast
+                .Where(x => x.Id == objWeatherForecast.Id)
+                .FirstOrDefault();
+            if (ExistingWeatherForecast != null)
+            {
+                ExistingWeatherForecast.Date =
+                    objWeatherForecast.Date;
+                ExistingWeatherForecast.Summary =
+                    objWeatherForecast.Summary;
+                ExistingWeatherForecast.TemperatureC =
+                    objWeatherForecast.TemperatureC;
+                ExistingWeatherForecast.TemperatureF =
+                    objWeatherForecast.TemperatureF;
+                _context.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
+
+        public Task<bool> DeleteForecastAsync(WeatherForecast objWeatherForecast)
+        {
+            var ExistingWeatherForecast =
+                _context.WeatherForecast
+                .Where(x => x.Id == objWeatherForecast.Id)
+                .FirstOrDefault();
+            if (ExistingWeatherForecast != null)
+            {
+                _context.WeatherForecast.Remove(ExistingWeatherForecast);
+                _context.SaveChanges();
+            }
+            else
+            {
+                return Task.FromResult(false);
+            }
+            return Task.FromResult(true);
+        }
     }
 }
---
+++ Pages/FetchData.razor.cs
@@ -0,0 +1,94 @@
+﻿
+using BlazorTest.Data.BlazorTest;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorTest.Pages
+{
+    public partial class FetchData
+    {
+        // AuthenticationState is available as a CascadingParameter
+        [CascadingParameter]
+        private Task<AuthenticationState> authenticationStateTask { get; set; }
+        List<WeatherForecast> forecasts;
+        protected override async Task OnInitializedAsync()
+        {
+            // Get the current user
+            var user = (await authenticationStateTask).User;
+            // Get the forecasts for the current user
+            // We access WeatherForecastService using @Service
+            forecasts = await @Service.GetForecastAsync(user.Identity.Name);
+        }
+        WeatherForecast objWeatherForecast = new WeatherForecast();
+        bool ShowPopup = false;
+        void ClosePopup()
+        {
+            // Close the Popup
+            ShowPopup = false;
+        }
+        void AddNewForecast()
+        {
+            // Make new forecast
+            objWeatherForecast = new WeatherForecast();
+            // Set Id to 0 so we know it is a new record
+            objWeatherForecast.Id = 0;
+            // Open the Popup
+            ShowPopup = true;
+        }
+        async Task SaveForecast()
+        {
+            // Close the Popup
+            ShowPopup = false;
+            // Get the current user
+            var user = (await authenticationStateTask).User;
+            // A new forecast will have the Id set to 0
+            if (objWeatherForecast.Id == 0)
+            {
+                // Create new forecast
+                WeatherForecast objNewWeatherForecast = new WeatherForecast();
+                objNewWeatherForecast.Date = System.DateTime.Now;
+                objNewWeatherForecast.Summary = objWeatherForecast.Summary;
+                objNewWeatherForecast.TemperatureC =
+                Convert.ToInt32(objWeatherForecast.TemperatureC);
+                objNewWeatherForecast.TemperatureF =
+                Convert.ToInt32(objWeatherForecast.TemperatureF);
+                objNewWeatherForecast.UserName = user.Identity.Name;
+                // Save the result
+                var result =
+                @Service.CreateForecastAsync(objNewWeatherForecast);
+            }
+            else
+            {
+                // This is an update
+                var result =
+                @Service.UpdateForecastAsync(objWeatherForecast);
+            }
+            // Get the forecasts for the current user
+            forecasts =
+            await @Service.GetForecastAsync(user.Identity.Name);
+        }
+        void EditForecast(WeatherForecast weatherForecast)
+        {
+            // Set the selected forecast
+            // as the current forecast
+            objWeatherForecast = weatherForecast;
+            // Open the Popup
+            ShowPopup = true;
+        }
+        async Task DeleteForecast()
+        {
+            // Close the Popup
+            ShowPopup = false;
+            // Get the current user
+            var user = (await authenticationStateTask).User;
+            // Delete the forecast
+            var result = @Service.DeleteForecastAsync(objWeatherForecast);
+            // Get the forecasts for the current user
+            forecasts =
+            await @Service.GetForecastAsync(user.Identity.Name);
+        }
+    }
+}
